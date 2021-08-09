a341d1e80e46b9f237be695774344ad720d2037a
Harshit Shah <hshah8@ext.uber.com>
2021-08-09 19:03:10 +0530

    updated backend (#1)

--- client/src/components/app-bar/AppBarComponent.js
+++ client/src/components/app-bar/AppBarComponent.js
@@ -1,13 +1,14 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const AppBarComponent = () => {
-    return (
-        <AppBar position="static" style={{ marginBottom: "70px" }}>
-            <Toolbar>
-                <Typography variant="h5">
-                    Expense Manager
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
-}
+  return (
+    <AppBar position="static" style={{ marginBottom: "70px" }}>
+      <Link to="/transactions" />
+      <Toolbar>
+        <Typography variant="h5">Expense Manager</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
